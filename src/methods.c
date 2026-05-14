e42351cc6a8b5f017b13a24fa491286af45f7c4c
Mohammed1768 <mohammedwissam728@gmail.com>
2026-05-14 20:50:05 +0300

    meow

--- src/methods.c
+++ src/methods.c
@@ -1,276 +1,49 @@
-#include "include.h"
+#include "../include/include.h"
+
+extern int8_t GPRS[GPRS_NUM];
+extern int8_t SREG;
+extern int8_t opcode;
+extern int8_t R1;
+extern int8_t R2_imm;
+
 
 void add(){
-    int negativeFlag=0;
-    int OverflowFlag=0;
-    int original8thBitR1=(R1>>7);
-    int original8thBitR2=(R2_imm>>7);
-    int answer=R1+R2_imm;
-    /*
-        Check for carry
-    */
-    int carryCheck=(answer>>8);
-    if(carryCheck==1){
-        SREG|=(1<<4);
-        NegativeFlag=1;
-    }
-    else{
-        SREG&=(~(1<<4));
-        NegativeFlag=0;
-    }
-
-    int result8thBit=answer>>7;
-
-/*
-    Check for overflow
-*/
-if(original8thBitR1==original8thBitR2){
-    if(result8thBit==original8thBitR1){
-        SREG&=(~(1<<3));
-        OverflowFlag=1;
-    }
-    else{
-        SREG|=(1<<3);
-        OverflowFlag=0;
-    }
+    GPRS[R1] += GPRS[R2_imm];
 }
-
-/*
-    Check for negative result
-*/
-if(result8thBit==1){
-    SREG|=(1<<2);
-}
-else{
-    SREG&=(~(1<<2));
-}
-/*
-    Check for Sign Bit
-*/
-if(OverflowFlag^NegativeFlag==1){
-    SREG|=(1<<1);
-}
-else{
-    SREG&=(~(1<<1));
-}
-
-/*
-    Check for Zero Flag
-*/
-if(result==0)
-{
-    SREG|=(1<<0);
-}
-else{
-    SREG&=(~(1<<0));
-}
-
-}
-
 void sub(){
-    int negativeFlag=0;
-    int OverflowFlag=0;
-    int original8thBitR1=(R1>>7);
-    int original8thBitR2=(R2_imm>>7);
-    int answer=R1-R2_imm;
-    /*
-        Check for carry
-    */
-    int carryCheck=(answer>>8);
-    if(carryCheck==1){
-        SREG|=(1<<4);
-        NegativeFlag=1;
-    }
-    else{
-        SREG&=(~(1<<4));
-        NegativeFlag=0;
-    }
-    /*
-        Check for overflow
-    */
-    int result8thBit=answer>>7;
-    if(result8thBit==original8thBitR2){
-        SREG&=(~(1<<3));
-        OverflowFlag=1;
-    }
-    else{
-        SREG|=(1<<3);
-        OverflowFlag=0;
-    }
-    /*
-    Check for negative result
-*/
-    if(result8thBit==1){
-        SREG|=(1<<2);
-    }
-    else{
-        SREG&=(~(1<<2));
-    }
-/*
-     Check for Sign Bit
-*/
-if(OverflowFlag^NegativeFlag==1){
-    SREG|=(1<<1);
+    GPRS[R1] -= GPRS[R2_imm];
 }
-else{
-    SREG&=(~(1<<1));
-}
-
-/*
-    Check for Zero Flag
-*/
-if(result==0)
-{
-    SREG|=(1<<0);
-}
-else{
-    SREG&=(~(1<<0));
-}
-/*
-    Need to store result in a REGISTER!!
-*/
-}
-
 void mul(){
-    int answer=R1*R2_imm;
-    /*
-        Update Negative Flag
-    */
-   int result8thBit=answer>>7;
-   if(result8thBit==1){
-        SREG|=(1<<2);
-    }
-    else{
-        SREG&=(~(1<<2));
-    }
-    /*
-        Check for Zero Flag
-    */
-    if(result==0)
-    {
-        SREG|=(1<<0);
-    }
-    else{
-        SREG&=(~(1<<0));
-    }
+    GPRS[R1] *= GPRS[R2_imm];
 }
-
 void ldi(){
-    R1=R2_imm;
+    GPRS[R1] = R2_imm;
 }
-
 void beqz(){
-    if(R1==0){
+    if(GPRS[R1] == 0){
         flush();
-        PC=PC+R2_imm;
+        PC += R2_imm;
     }
 }
-
 void and(){
-    int answer=R1&R2_imm;
-    /*
-        Update Negative Flag
-    */
-   int result8thBit=answer>>7;
-   if(result8thBit==1){
-        SREG|=(1<<2);
-    }
-    else{
-        SREG&=(~(1<<2));
-    }
-    /*
-        Check for Zero Flag
-    */
-    if(result==0)
-    {
-        SREG|=(1<<0);
-    }
-    else{
-        SREG&=(~(1<<0));
-    }
+    GPRS[R1] &= GPRS[R2_imm];
 }
-
 void or(){
-    int answer=R1|R2_imm;
-    /*
-        Update Negative Flag
-    */
-   int result8thBit=answer>>7;
-   if(result8thBit==1){
-        SREG|=(1<<2);
-    }
-    else{
-        SREG&=(~(1<<2));
-    }
-    /*
-        Check for Zero Flag
-    */
-    if(result==0)
-    {
-        SREG|=(1<<0);
-    }
-    else{
-        SREG&=(~(1<<0));
-    }
+    GPRS[R1] |= GPRS[R2_imm];
 }
-
 void jr(){
     PC = (R1 << 6) | (R2_imm);
     flush();
 }
-
 void sal(){
-    R1=R1<<R2_imm;
-    /*
-        Update Negative Flag
-    */
-    int result8thBit=R1>>7;
-    if(result8thBit==1){
-          SREG|=(1<<2);
-     }
-     else{
-          SREG&=(~(1<<2));
-     }
-        /*
-            Check for Zero Flag
-        */
-        if(result==0)
-        {
-            SREG|=(1<<0);
-        }
-        else{
-            SREG&=(~(1<<0));
-        }
+    GPRS[R1] <<= R2_imm; 
 }
-
 void sar(){
-    GPRS[R1] >>= R2_imm;
-    /*
-        Update Negative Flag
-    */
-    int result8thBit=R1>>7;
-    if(result8thBit==1){
-          SREG|=(1<<2);
-     }
-     else{
-          SREG&=(~(1<<2));
-     }
-        /*
-            Check for Zero Flag
-        */
-        if(result==0)
-        {
-            SREG|=(1<<0);
-        }
-        else{
-            SREG&=(~(1<<0));
-        }
+    GPRS[R1] >>= R2_imm; 
 }
-
 void lb(){
     GPRS[R1] = get_data(R2_imm);
 }
-
 void sb(){
     set_data(R2_imm, GPRS[R1]);
 }
