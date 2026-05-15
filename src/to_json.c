feb54d00acdc9c9c4c9bd412b054280ce65bccf7
Mohammed1768 <mohammedwissam728@gmail.com>
2026-05-15 17:46:08 +0300

    meow

---
+++ src/to_json.c
@@ -0,0 +1,30 @@
+
+#include "../include/include.h"
+
+void write_data_memory() {
+
+}
+
+void write_instruction_memory() {
+
+}
+
+void write_SREG() {
+
+}
+
+void write_registers() {
+
+}
+
+void write_clk_PC() {
+
+}
+
+void write_everyting_to_json() {
+    write_data_memory();
+    write_instruction_memory();
+    write_SREG();
+    write_registers();
+    write_clk_PC();
+}
